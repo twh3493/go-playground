7efadd69954d07e3fc76ebf6d59442f4cb284bf7
Vita Huang <vita94203@gmail.com>
2023-12-13 21:46:42 +0800

    refactor prime-numbers

--- prime-numbers/main.go
+++ prime-numbers/main.go
@@ -3,18 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("HW-1 Prime Numbers.")
-	for a := 2; a <= 100; a++ {
-		var c bool = true
-		for b := 2; b < a-1; b++ {
-			if a%b == 0 {
-				c = false
-
-			}
-		}
-		if c == true {
-			fmt.Println("", a)
-
+	// fmt.Println("HW-1 Prime Numbers.")
+	for a := 2; a <= 100; a = a + 1 {
+		if isPrime(a) {
+			fmt.Println(a)
 		}
 	}
+}
+func isPrime(x int) bool {
+	for b := 2; b < x-1; b++ {
+		if x%b == 0 {
+			return false
+		}
+
+	}
+	return true
 }
